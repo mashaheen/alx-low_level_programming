77de57a94381d390f143a52d5ec4d5fcdd13e92e
mashaheen <finalflash147@yahoo.com>
2023-08-11 16:10:42 -0700

    a

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: zero
+ */
+int main(void)
+{
+	int chr;
+
+	for (chr = 0; chr < 10; chr++)
+	{
+		putchar(chr);
+	}
+	putchar('\n');
+	return (0);
+}
