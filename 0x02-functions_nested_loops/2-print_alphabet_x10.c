0d343d6056cbd0378a62b6e2fbcd40cd32b23927
mashaheen <finalflash147@yahoo.com>
2023-08-15 16:48:43 -0700

    a

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include"main.h"
+/**
+ * print_alphabet_x10 - print alphabet
+ *
+ * Return : void
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		print_alphabet();
+	}
+	putchar('\n');
+}
