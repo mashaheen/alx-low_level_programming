c2d2f6bd318e36b2555227d079387a2d1e09f160
mashaheen <finalflash147@yahoo.com>
2023-08-17 14:12:15 -0700

    d

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,15 @@
+#include"main.h"
+/**
+ * _isdigit - print alphabet
+ * @c: argument description
+ *
+ * Return: 1 if is alphaet, and z otherwise
+ */
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+	{
+		return (1);
+	}
+	return (0);
+}
