8a5c70728c4ccbb93d39dcca503393fb900db6b1
mashaheen <finalflash147@yahoo.com>
2023-08-17 13:50:03 -0700

    a

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include"main.h"
+/**
+ * _isupper - print alphabet
+ * @c : argument description
+
+ * Return : int
+ */
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	} else
+	{
+		return (0);
+	}
+}
