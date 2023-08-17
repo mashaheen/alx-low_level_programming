ff6bae59599c9f11d8e4c1f7802af496c854c221
mashaheen <finalflash147@yahoo.com>
2023-08-17 13:59:04 -0700

    a

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,16 +1,16 @@
 #include"main.h"
 /**
  * _isupper - print alphabet
- * @c : argument description
-
- * Return : int
+ * @c: argument description
+ *
+ * Return: 1 if is alphaet, and z otherwise
  */
 int _isupper(int c)
 {
 	if (c >= 'A' && c <= 'Z')
 	{
 		return (1);
-	} else
+	}else
 	{
 		return (0);
 	}
