5cfff0c3c44d4215b3416b74a45379bd108e4a27
mashaheen <finalflash147@yahoo.com>
2023-08-21 16:58:01 -0700

    a

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -9,9 +9,12 @@
 void puts2(char *str)
 {
 	do {
+		if (*str == '\0')
+		{
+			break;
+		}
 		_putchar(*str);
 		str = str + 2;
-	}
-	while (*str != '\0' && *(str - 1) != '\0');
+	} while (*str != '\0' && *(str - 1) != '\0');
 	_putchar('\n');
 }
