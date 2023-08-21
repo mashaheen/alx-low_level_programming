233d13efc683a6ae14c37afa5ac584f7d358c92e
mashaheen <finalflash147@yahoo.com>
2023-08-21 16:07:55 -0700

    a

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include"main.h"
+/**
+ * _puts - print alphabet
+ * @str: argument desc
+ *
+ * Return: void
+ */
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str);
+		str++;
+	}
+	_putchar('\n');
+}
