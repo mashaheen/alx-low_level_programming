f277998a5f6351964922776a7e380a862087f02f
mashaheen <finalflash147@yahoo.com>
2023-08-24 13:18:16 -0700

    a

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,32 @@
+#include"main.h"
+/**
+ * _strncpy - print alphabet
+ * @dest: argument description
+ * @src: argument
+ * @n: number of letters in second string
+ *
+ * Return: pointer to newchar
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	char *des = dest;
+	char *sr = src;
+	int i = 0;
+
+	while (*sr != '\0')
+	{
+		if (i >= n)
+		{
+			*des = '*';
+		} else
+		{
+			*des = *sr;
+		}
+		sr++;
+		des++;
+		i++;
+	}
+	des = dest;
+
+	return (des);
+}
