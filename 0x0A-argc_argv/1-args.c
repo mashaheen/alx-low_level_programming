e259885db90e51f32ea941778f5691cabaadc4c1
mashaheen <finalflash147@yahoo.com>
2023-09-01 17:15:38 -0700

    a

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+/**
+ * main - entry point
+ * @argc: number of arguemnts
+ * @argv: array of pointers
+ *
+ * Return: zero
+ */
+int main(int argc, char *argv[])
+{
+	if (argc > 0)
+	{
+		printf("%d\n", argc);
+	}
+	return (0);
+}
