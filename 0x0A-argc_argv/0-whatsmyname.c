24e3af49c40d7a4fd8e6b6988cdda3772c67f9a8
mashaheen <finalflash147@yahoo.com>
2023-09-01 17:12:19 -0700

    a

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+/**
+ * main - entry point
+ * @argc: number of arguemnts
+ * @argv: array of pointers
+ *
+ * Return: zero
+ */
+int main(int argc, char *argv[])
+{
+	if (argc != 0)
+	{
+		print_f("%s\n", argv[0]);
+	}
+	return (0);
+}
