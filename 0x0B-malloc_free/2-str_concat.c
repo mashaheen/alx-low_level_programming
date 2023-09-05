a344a98c9821f310ce5625e0da7bb6a5cf3e688b
mashaheen <finalflash147@yahoo.com>
2023-09-05 11:33:29 -0700

    a

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,37 @@
+#include"main.h"
+#include<stdlib.h>
+#include<string.h>
+/**
+ * str_concat - print alphabet
+ * @s1: argument description
+ * @s2: argument description
+ *
+ * Return: pointer to newchar
+ */
+char *str_concat(char *s1, char *s2)
+{
+	int i;
+	int s1Size;
+	int s2SSize;
+	char *array;
+
+	s1Size = strlen(s1);
+	s2Size = strlen(s2);
+
+	array = malloc(sizeof(char) * (s1Size+s2Size));
+	if (array == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < s1Size; i++)
+	{
+		array[i] =  s1[i];
+	}
+	for (i = 0; i < s2Size; i++)
+	{
+		array[i + s1Size] = s2[i];
+	}
+	return (array);
+
+}
