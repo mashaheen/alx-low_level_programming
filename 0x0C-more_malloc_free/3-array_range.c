09d3e83c26f5e832a2a2bc8d0fab8cb9c52107b2
mashaheen <finalflash147@yahoo.com>
2023-09-08 01:29:16 -0700

    a

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,33 @@
+#include"main.h"
+/**
+ * array_arrange - func
+ * @min: argument description
+ * @max: argument description
+ *
+ * Return: return
+ */
+int *array_range(int min, int max)
+{
+	int *p;
+	int size;
+       	int i;
+
+	if (min > max)
+	{
+		return (NULL);
+	}
+	size = (max - min) + 1;
+	p = malloc(size);
+	if (p == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < size; i++)
+	{
+		p[i] = min;
+		min++;
+	}
+	return (p);
+
+}
