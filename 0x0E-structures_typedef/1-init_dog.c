0a7c724922dbe5956b188e6ba506bcd1c04e6c42
mashaheen <finalflash147@yahoo.com>
2023-09-12 12:25:14 -0700

    a

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include "dog.h"
+/**
+  * init_dog - func
+  * @d: dog
+  * @name: dog name
+  * @age: dog age
+  * @owner: dog owner
+  *
+  * Return: void
+  */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d != NULL)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+}
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -14,4 +14,6 @@ typedef struct dog
 	float age;
 	char *owner;
 } dog;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
 #endif
