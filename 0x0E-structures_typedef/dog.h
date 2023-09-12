2e0fc181ea56e1d5684872c9e77c2f205d08af72
mashaheen <finalflash147@yahoo.com>
2023-09-12 11:56:06 -0700

    a

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,7 +1,17 @@
-
-
-struct dog {
+#ifndef DOG_A
+#define DOG_A
+/**
+ * struct dog - dog 
+ * @name: name 
+ * @age: age 
+ * @owner: owner
+ *
+ * Description: Defines dog
+ */
+typedef struct dog
+{
 	char *name;
 	float age;
 	char *owner;
-}
+} dog;
+#endif
