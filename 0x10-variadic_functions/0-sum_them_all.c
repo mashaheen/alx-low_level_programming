6476906863ac374661a685af1a0a41e24b0194c4
mashaheen <finalflash147@yahoo.com>
2023-09-14 13:25:17 -0700

    a

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,26 @@
+#include"main.h"
+/**
+ * sum_them_all - get the sum of all args
+ * @n: number of arguments
+ *
+ * Return: sum of all arguments
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list args;
+	int i;
+	int sum;
+
+	if (n == 0)
+		return (0);
+
+	va_start(args, n);
+	sum = 0;
+	for (i = 0; i < n; i++)
+	{
+		int x = va_arg(args, int);
+
+		sum = sum + x;
+	}
+	return (sum);
+}
---
+++ 0x10-variadic_functions/main.h
@@ -0,0 +1,5 @@
+#include<stdio.h>
+#include<stdarg.h>
+#include<string.h>
+
+int sum_them_all(const unsigned int n, ...);
