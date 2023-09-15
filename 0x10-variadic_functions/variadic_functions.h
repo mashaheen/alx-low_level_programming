f96a14475995240fade4d446016899afe247c7d4
mashaheen <finalflash147@yahoo.com>
2023-09-15 14:13:34 -0700

    a

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -5,3 +5,8 @@
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+void p_char(va_list c);
+void p_int(va_list i);
+void p_float(va_list f);
+void p_string(va_list s);
+void print_all(const char * const format, ...);
