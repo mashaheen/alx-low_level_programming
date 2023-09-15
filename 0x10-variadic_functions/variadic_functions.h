3aa144c5038fdad14df6db5c63b84829962190b6
mashaheen <finalflash147@yahoo.com>
2023-09-15 15:47:42 -0700

    a

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -2,6 +2,13 @@
 #include<stdarg.h>
 #include<string.h>
 
+typedef struct validTypes
+{
+	char *valid;
+	void (*f)();
+} v_types;
+
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
